d6beee2ec67ef8725fc481649e92273af5d0593f
Wataru Maeda <w.maeda.jp@gmail.com>
2024-03-02 13:22:23 -0800

    chore: update button

--- src/views/Details/Details.tsx
+++ src/views/Details/Details.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, StyleSheet, StatusBar } from 'react-native';
-import Button from '@components/Button';
+import GradientButton from '@components/GradientButton';
 import { StackProps } from '@navigator/stack';
 import { colors } from '@theme';
 
@@ -31,16 +31,20 @@ const styles = StyleSheet.create({
 
 export default function Details({ navigation, route }: StackProps) {
   const { from } = route.params;
+
   return (
     <View style={styles.root}>
       <StatusBar barStyle="light-content" />
       <Text style={styles.title}>{`Details (from ${from})`}</Text>
-      <Button
+      <GradientButton
         title="Go back to Home"
         titleStyle={styles.buttonTitle}
         style={styles.button}
-        onPress={() => {
-          navigation.goBack();
+        onPress={() => navigation.goBack()}
+        gradientBackgroundProps={{
+          colors: [colors.purple, colors.pink],
+          start: { x: 0, y: 1 },
+          end: { x: 0.8, y: 0 },
         }}
       />
     </View>
