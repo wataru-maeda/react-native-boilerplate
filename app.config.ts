6ea5020b56df239016bb2a2e552c192fbecfb84d
Wataru Maeda <w.maeda.jp@gmail.com>
2024-05-03 10:16:21 -0700

    chore: update env variables

--- app.config.ts
+++ app.config.ts
@@ -1,73 +1,29 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
-export default ({ config: expoConfig }: ConfigContext): ExpoConfig => {
-  let name = '';
-  let slug = '';
-  let projectId = '';
-  let ios = { bundleIdentifier: '', buildNumber: '' };
-  let android = { package: '', versionCode: 1 };
-
-  /**
-   * dev configuration
-   */
-  function setDevConfig() {
-    name = `DEV - ${expoConfig.name}`;
-    slug = 'react-native-boilerplate-dev';
-    projectId = '3a2f44a2-b2cb-4445-91e0-5e41dad0b0c4';
-    ios = {
-      bundleIdentifier: 'com.watarumaeda.react-native-boilerplate-dev',
-      buildNumber: '1.0.0',
-    };
-    android = {
-      package: 'com.watarumaeda.react_native_boilerplate.dev',
-      versionCode: 1,
-    };
-  }
-
-  /**
-   * prod configuration
-   */
-  function setProdConfig() {
-    name = expoConfig.name ?? '';
-    slug = 'react-native-boilerplate';
-    projectId = '18adc0d0-eb1d-11e9-8009-d524ed5cc4a7';
-    ios = {
-      bundleIdentifier: 'com.watarumaeda.react-native-boilerplate',
-      buildNumber: '1.0.0',
-    };
-    android = {
-      package: 'com.watarumaeda.react_native_boilerplate',
-      versionCode: 1,
-    };
-  }
-
-  // switch expo configuration based on environment
-  const targetEnv = process.env.NODE_ENV;
-  if (targetEnv === 'production') setProdConfig();
-  else setDevConfig();
-
+export default ({ config }: ConfigContext): ExpoConfig => {
   const envConfig: ExpoConfig = {
-    ...expoConfig,
-    name,
-    slug,
-    updates: {
-      url: `https://u.expo.dev/${projectId}`,
-    },
+    ...config,
+    slug: process.env.EXPO_PUBLIC_SLUG ?? '',
+    name: process.env.EXPO_PUBLIC_NAME ?? '',
     ios: {
-      ...expoConfig.ios,
-      ...ios,
+      ...config.ios,
+      bundleIdentifier: process.env.EXPO_PUBLIC_IOS_BUNDLE_IDENTIFIER,
+      buildNumber: '1',
     },
     android: {
-      ...expoConfig.android,
-      ...android,
+      ...config.android,
+      package: process.env.EXPO_PUBLIC_IOS_ANDROID_PACKAGE,
+      versionCode: 1,
+    },
+    updates: {
+      url: `https://u.expo.dev/${process.env.EXPO_PUBLIC_PROJECT_ID}`,
     },
     extra: {
-      ...expoConfig.extra,
-      eas: { projectId },
+      ...config.extra,
+      eas: { projectId: process.env.EXPO_PUBLIC_PROJECT_ID },
+      ENV: process.env.EXPO_PUBLIC_ENV,
+      API_URL: process.env.EXPO_PUBLIC_API_URL,
     },
   };
-
-  // console.log('[##] your expo config', envConfig);
-
   return envConfig;
 };
