ddd18bd3954d43f22b8c4f75718128900f472de0
Wataru Maeda <wataru.maeda@dotdata.com>
2024-12-25 20:21:02 -0800

    chore: configure typescript

---
+++ react-native-boilerplate/app.config.ts
@@ -0,0 +1,9 @@
+import 'ts-node/register'; // Add this to import TypeScript files
+import { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: 'my-app',
+  slug: 'my-app',
+};
+
+export default config;
