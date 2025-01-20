70a98b495dc55ed96030bda8a89b5adab3f6dc9d
Wataru Maeda <wataru.maeda@dotdata.com>
2025-01-20 15:59:02 -0800

    chore: remove dotenvx sicne web version not working

--- react-native-boilerplate/utils/config.ts
+++ react-native-boilerplate/utils/config.ts
@@ -1,6 +1,8 @@
 import Constants from 'expo-constants';
 import { Env } from '@/types'
 
+console.log('[##] process.env', Object.keys(process.env));
+
 const config = {
   env: Constants.expoConfig?.extra?.env as Env,
   apiUrl: Constants.expoConfig?.extra?.apiUrl as string,
